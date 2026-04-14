87f25b83d3651dc64f94a0eff19df99300f4ba25
U-12031 <moro.yoshi0902@gmail.com>
2026-04-14 21:05:40 +0900

    時計を追加

--- schoolTimeTable/script.js
+++ schoolTimeTable/script.js
@@ -1,1 +1,39 @@
 function el(id) {return document.getElementById(id)};
+
+let nowTime; // new Date()する変数
+let now = { // nowTimeから分離して使う関数
+	y: null,
+	mo: null,
+	d: null,
+	h: null,
+	mi: null,
+	s: null,
+	ms: null
+}
+
+function addZero(num, length=2) {
+	return num.toString().padStart(length, "0");
+}
+function nowUpdate() {
+	nowTime = new Date();
+	now.y = nowTime.getFullYear();
+	now.mo = nowTime.getMonth();
+	now.d = nowTime.getDate();
+	now.h = nowTime.getHours();
+	now.mi = nowTime.getMinutes();
+	now.s = nowTime.getSeconds();
+	now.ms = nowTime.getMilliseconds();
+}
+
+function timeUpdate() {
+	nowUpdate();
+	el("otherTime").innerHTML = addZero(now.h) + ":" + addZero(now.mi)
+	el("secondTime").innerHTML = "." + addZero(now.s);
+	el("secondTime").style.opacity = 1 - now.ms / 1000;
+	// el("secondTime").style.textDecorationColor = `hsl(0, 0%, ${now.ms / 10}%)`;
+	requestAnimationFrame(timeUpdate);
+}
+
+try {
+timeUpdate()
+} catch(error) {console.log(`${error.name}\n${error.stack}`)}
